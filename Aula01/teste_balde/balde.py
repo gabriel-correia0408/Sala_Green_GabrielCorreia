923c335a967962d4492deebf34fa17d0e342a35f
GABRIEL <gabrielmal38@gmail.com>
2020-03-02 11:32:41 -0300

    Aula dois exercicos com testes

---
+++ Aula01/teste_balde/balde.py
@@ -0,0 +1,7 @@
+from random import randint
+
+
+class Balde:
+    def __init__(self,):
+
+
---
+++ Aula01/teste_dobradura/dobradura.py
@@ -0,0 +1,54 @@
+# DOBRADURA de Papel
+#
+# Uma professora de arte pediu um programa para clacular quanto poderia dividir um papel com
+# uma quantidade de dobradura.
+#
+# Ela falou que com uma dobradura, poderia dividir o papel em 2 pedaços e com três dobraduras
+# pode-se dividir o papel em 8 pedaços.
+#
+# Faça uma classe que:
+# 1) calcule e retorne a quantidade de divisões do papel por uma quantidade de dobradura.
+# 2) A quantidade de dobraduras não pode ser numero negativo e nem aceitar string. Caso seja colocado
+#       numero negativo e caracteres a função deve retornar False
+#
+#
+# Atenção!!!
+# A estrutura do programa é a seguinte:
+# 1) Nome da classe: Dobradura.
+# 2) Nome do metodo: get_dobrar(self,numero_dobradura)
+#
+# Deve ser salvado com o nome do arquivo dobradura.py dentro da pasta teste_dobradura.
+# Deve ser executado o arquivo teste_de_dobradura.py e o mesmo deve aparecer a mensagem 'Teste Conculido com Sucesso!'
+
+class Dobradura:
+
+    def get_dobrar(self,numero_dobradura):
+        self.numero_dobradura = numero_dobradura
+        base_folha = 1
+        expoente_dobradura = self.numero_dobradura
+
+        while count(self.numero_dobradura) <= expoente_dobradura:
+            for i in base_folha:
+                a = (i ** 2)
+            else:
+                'false'
+
+
+
+
+
+if __name__ == '__main__':
+    a = Dobradura()
+a = Dobradura()
+print(a.get_dobrar(13))
+
+
+
+
+
+
+
+
+
+
+
