3fdffea5a8ea9bc0b137429ae4a91ee2f7f4ac9d
GabrielCorreia <gabrielmal38@gmail.com.br>
2020-05-10 11:33:20 -0300

    testando de forma básica a saida, do método, soma, com doctest

--- p_teste/calculadora.py
+++ p_teste/calculadora.py
@@ -1,3 +1,22 @@
 #criando função de nome soma
 def soma(x,y):
-    return x+y
+    """
+    soma x e y
+    >>> soma(20,20)
+    40
+
+    >>> soma(-10,50)
+    40
+
+    >>> soma('10',20)
+    Traceback (most recent call last):
+    ...
+    AssertionError: x precisa ser int ou float
+    """
+    assert isinstance(x,(int,float)), 'x precisa ser int ou float'
+    assert  isinstance(y, (int, float)), 'y precisa ser int ou float'
+    return x + y
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
