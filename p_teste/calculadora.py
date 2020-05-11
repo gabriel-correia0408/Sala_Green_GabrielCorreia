31e38e44ee3b237f01e579d244c97db32618ffde
GabrielCorreia <gabrielmal38@gmail.com.br>
2020-05-10 21:36:59 -0300

    iniciando em unittest

--- p_teste/calculadora.py
+++ p_teste/calculadora.py
@@ -14,9 +14,9 @@ def soma(x,y):
     AssertionError: x precisa ser int ou float
     """
     assert isinstance(x,(int,float)), 'x precisa ser int ou float'
-    assert  isinstance(y, (int, float)), 'y precisa ser int ou float'
+    assert isinstance(y, (int, float)), 'y precisa ser int ou float'
     return x + y
 
 if __name__ == '__main__':
     import doctest
-    doctest.testmod()
+    doctest.testmod(verbose=True)
